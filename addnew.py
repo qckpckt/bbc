27881c51b83b036288969c56b4c99f35f0268b7f
Alex Myers <alexmyers@Macintosh.local>
2016-12-30 12:09:46 -0800

    slight changes to constants

--- addnew.py
+++ addnew.py
@@ -3,8 +3,7 @@ import requests
 
 DEFAULT_HEADERS = {'Accept': 'application/json'}
 META_URL = 'https://api.bamboohr.com/api/gateway.php/mobify/v1/meta/fields/'
-CHANGED_SINCE = 'https://api.bamboohr.com/api/gateway.php/mobify/v1/employees/changed/?since={ISO 8601 timestamp}&type=inserted'
-ALEX_ID = '40597'
+CHANGED_SINCE = 'https://api.bamboohr.com/api/gateway.php/mobify/v1/employees/changed/?since={timestamp}&type=inserted'
 
 token = os.getenv('BAMBOO_API_KEY')
 
@@ -13,4 +12,4 @@ resp = requests.get(USE_CELL.format(employee_id=ALEX_ID), auth=auth, headers = D
 
 # reply = resp.json()
 
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
