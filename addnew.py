f84e6b1ee48be37df23ee26f5f2fd0d323cc67fd
Alex Myers <alexmyers@Macintosh.local>
2017-01-05 16:22:18 -0800

    built up the API call to return an array of employees who have been changed in the last 2 weeks

--- addnew.py
+++ addnew.py
@@ -1,16 +1,25 @@
 import os
 import requests
+import datetime
 
+BAMBOO_TOKEN = os.getenv('BAMBOO_API_KEY')
 DEFAULT_HEADERS = {'Accept': 'application/json'}
 META_URL = 'https://api.bamboohr.com/api/gateway.php/mobify/v1/meta/fields/'
-CHANGED_SINCE = 'https://api.bamboohr.com/api/gateway.php/mobify/v1/employees/changed/?since={timestamp}&type=inserted'
+CHANGED_SINCE_API = 'https://api.bamboohr.com/api/gateway.php/mobify/v1/employees/changed/?since={timestamp}&type=inserted'
 
 
-token = os.getenv('BAMBOO_API_KEY')
+def whats_new():
 
-auth = requests.auth.HTTPBasicAuth(token, 'x')
-resp = requests.get(CHANGED_SINCE.format(timestamp=LAST_UPDATE, auth=auth, headers = DEFAULT_HEADERS)
+    last_run = datetime.datetime.now() - datetime.timedelta(weeks=2)
+    time_stamp = str(last_run.isoformat()).split('.')[0]
+    date_plus_timezone = time_stamp + '-08.00'
+    url = CHANGED_SINCE_API.format(timestamp = date_plus_timezone)
+    auth = requests.auth.HTTPBasicAuth(BAMBOO_TOKEN, 'x')
+    resp = requests.get(url, auth=auth, headers = DEFAULT_HEADERS)
+    directory = resp.json()
+    return directory['employees']
 
-# reply = resp.json()
+if __name__ == '__main__':
+    whats_new()
 
 # import pdb; pdb.set_trace()
